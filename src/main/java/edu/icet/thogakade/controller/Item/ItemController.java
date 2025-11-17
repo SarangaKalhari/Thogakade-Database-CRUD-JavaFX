2dbaf561fdbb0089f189a4f70895d0ab170b4663
SarangaKalhari <kalharisaranga9@gmail.com>
2025-11-17 17:45:32 +0530

    Make function bar in Item Management UI

--- src/main/java/edu/icet/thogakade/controller/Item/ItemController.java
+++ src/main/java/edu/icet/thogakade/controller/Item/ItemController.java
@@ -1,12 +1,17 @@
 package edu.icet.thogakade.controller.Item;
 
+import edu.icet.thogakade.controller.DashboardFormController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
+
 public class ItemController {
+
+    DashboardFormController formController = new DashboardFormController();
 
     @FXML
     private TableColumn<?, ?> colDescription;
@@ -62,22 +67,25 @@ public class ItemController {
     }
 
     @FXML
-    void customerOnAction(ActionEvent event) {
+    void customerOnAction(ActionEvent event) throws IOException {
+        formController.customerOnAction(event);
+    }
+
+    @FXML
+    void itemOnAction(ActionEvent event) throws IOException {
+        formController.itemOnAction(event);
 
     }
 
     @FXML
-    void itemOnAction(ActionEvent event) {
+    void orderDetailOnAction(ActionEvent event) throws IOException {
+        formController.orderDetailOnAction(event);
 
     }
 
     @FXML
-    void orderDetailOnAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void orderOnAction(ActionEvent event) {
+    void orderOnAction(ActionEvent event) throws IOException {
+        formController.orderOnAction(event);
 
     }
 
